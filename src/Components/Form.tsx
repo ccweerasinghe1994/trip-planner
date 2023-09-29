3585a45eb0ef7399a541d4a6b3128449fd131e95
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-09-29 14:47:37 +0530

    feat: adding authentication

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,8 +1,43 @@
+import { FormEvent, useState } from 'react';
+
 const Form = () => {
+	const [quantity, setQuantity] = useState(1);
+	const [description, setDescription] = useState('');
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const newItem = {
+			id: Math.random() * 1000,
+			description,
+			quantity,
+			packed: false,
+		};
+		setQuantity(1);
+		setDescription('');
+	};
+	const handleChange = (e: FormEvent<HTMLSelectElement | HTMLInputElement>) => {
+		const { value, name } = e.currentTarget;
+		if (name === 'description') setDescription(value);
+		if (name === 'quantity') setQuantity(Number(value));
+	};
 	return (
-		<div className={'add-form'}>
+		<form className={'add-form'} onSubmit={handleSubmit}>
 			<h3>what do you need for your 😍 trip ?</h3>
-		</div>
+			<select name={'quantity'} value={quantity} onChange={handleChange}>
+				{Array.from({ length: 10 }, (_, i) => i + 1).map((item) => (
+					<option key={item} value={item}>
+						{item}
+					</option>
+				))}
+			</select>
+			<input
+				name={'description'}
+				onChange={handleChange}
+				value={description}
+				type="text"
+				placeholder={'item...'}
+			/>
+			<button>add</button>
+		</form>
 	);
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import {
+	ClerkProvider,
+	RedirectToSignIn,
+	SignedIn,
+	SignedOut,
+} from '@clerk/clerk-react';
 
+if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
+	throw new Error('Missing Publishable Key');
+}
+const clerkPubKey: string = import.meta.env
+	.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<ClerkProvider publishableKey={clerkPubKey}>
+		<React.StrictMode>
+			<SignedIn>
+				<App />
+			</SignedIn>
+			<SignedOut>
+				<RedirectToSignIn />
+			</SignedOut>
+		</React.StrictMode>
+	</ClerkProvider>,
+);
